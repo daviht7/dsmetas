7bf6267ed32d78604129dc1f98212606ee7680d0
Davi Holanda <daviht7@gmail.com>
2022-07-12 20:35:01 -0300

    componente notification button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { NotificationButton } from "./components/NotificationButton";
 
 function App() {
-  return <h1>Olá mundo!</h1>;
+  return (
+    <>
+      <h1>ola</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
